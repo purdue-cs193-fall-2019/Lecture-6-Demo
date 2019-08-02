161fc2fea2e45abb61645524d53d8038b481efe4
CJ Enright <cenrigh@purdue.edu>
2019-08-02 18:26:21 -0500

    Add initial content

---
+++ Memes/HelloWorld.java
@@ -0,0 +1,9 @@
+/* This program prints "Hello, World" */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
